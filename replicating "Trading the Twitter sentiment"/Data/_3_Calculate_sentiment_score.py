75f4bc156a8ee7b30c87085e395e287493712bec
fabianwin <fabian.winkelmann@outlook.com>
2021-09-30 07:44:08 +0200

    add several sentiment scores

--- replicating "Trading the Twitter sentiment"/Data/_3_Calculate_sentiment_score.py
+++ replicating "Trading the Twitter sentiment"/Data/_3_Calculate_sentiment_score.py
@@ -4,6 +4,11 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pandas as pd
+import numpy as np
+import flair
+from flair.data import Sentence
+from textblob import TextBlob
+
 
 def preprocess_tweet(tweet):
     # Remove urls
@@ -26,11 +31,69 @@ stanza.download('en')
 print("Building an English pipeline...")
 en_nlp = stanza.Pipeline('en')
 
-#---------------------
-import numpy as np
-import flair
-from flair.data import Sentence
 
+#----------------------------------------------
+def get_stanford_sentiment(twitter_df):
+    """
+    - Parameters: twitter_df, twitter_df has all the tweets info stored
+    - Returns: df_final, same shape as twitter_df but with the calculated scores
+    """
+
+    Stanford_sentiment = []
+    for index, row in twitter_df.iterrows():
+        text = Sentence(preprocess_tweet(row['content']))
+        text = str(text)
+        text = en_nlp(text)
+        score = sentence_sentiment_df(text)
+        Stanford_sentiment.append(score)
+    twitter_df['Stanford_sentiment'] = Stanford_sentiment
+
+    return twitter_df
+
+#----------------------------------------------
+def get_textblob_sentiment(twitter_df):
+    """
+    - Parameters: twitter_df, twitter_df has all the tweets info stored
+    - Returns: df_final, same shape as twitter_df but with the calculated scores
+    """
+
+    Textblob_sentiment = []
+    for index, row in twitter_df.iterrows():
+        text = Sentence(preprocess_tweet(row['content']))
+        text = str(text)
+        score = TextBlob(text).sentiment.polarity
+        Textblob_sentiment.append(score)
+    twitter_df['TextBlob_sentiment'] = Textblob_sentiment
+
+    return twitter_df
+
+#----------------------------------------------
+def get_flair_sentiment(twitter_df):
+    """
+    - Parameters: twitter_df, twitter_df has all the tweets info stored
+    - Returns: df_final, same shape as twitter_df but with the calculated scores
+    """
+
+    sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+    probs = []
+    sentiments = []
+    flair_sentiment=[]
+    for index, row in twitter_df.iterrows():
+        sent=0
+        sentence = flair.data.Sentence(row['content'])
+        sentiment_model.predict(sentence)
+        # extract sentiment prediction
+        probs.append(sentence.labels[0].score)  # numerical score 0-1
+        sentiments.append(sentence.labels[0].value)  # 'POSITIVE' or 'NEGATIVE'
+        if sentence.labels[0].value=="POSITIVE":
+            sent = sentence.labels[0].score
+        else :
+            sent =1-sentence.labels[0].score
+        flair_sentiment.append(sent)
+    twitter_df['Flair_sentiment'] = flair_sentiment
+
+    return twitter_df
+#----------------------------------------------
 def sentence_sentiment_df(doc):
     """
     - Parameters: doc (a Stanza Document object)
@@ -43,57 +106,41 @@ def sentence_sentiment_df(doc):
 
     mean_sentiment = np.mean(sentiment_values)
     return mean_sentiment
+#----------------------------------------------
+
+
 
 # Calculate TSLA tweets
 ticker_tweets_TSLA = pd.read_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/ticker_set_TSLA.csv')
 product_tweets_TSLA = pd.read_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/product_set_TSLA.csv')
 
-Stanford_sentiment = []
-for index, row in ticker_tweets_TSLA.iterrows():
-    text = Sentence(preprocess_tweet(row['content']))
-    text = str(text)
-    text = en_nlp(text)
-    score = sentence_sentiment_df(text)
-    Stanford_sentiment.append(score)
+get_stanford_sentiment(ticker_tweets_TSLA)
+get_textblob_sentiment(ticker_tweets_TSLA)
+get_flair_sentiment(ticker_tweets_TSLA)
 
-ticker_tweets_TSLA['Stanford_sentiment'] = Stanford_sentiment
+get_stanford_sentiment(product_tweets_TSLA)
+get_textblob_sentiment(product_tweets_TSLA)
+get_flair_sentiment(product_tweets_TSLA)
+
 ticker_tweets_TSLA.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/ticker_set_TSLA.csv', index = False)
-
-Stanford_sentiment = []
-for index, row in product_tweets_TSLA.iterrows():
-    text = Sentence(preprocess_tweet(row['content']))
-    text = str(text)
-    text = en_nlp(text)
-    score = sentence_sentiment_df(text)
-    Stanford_sentiment.append(score)
-
-product_tweets_TSLA['Stanford_sentiment'] = Stanford_sentiment
 product_tweets_TSLA.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/product_set_TSLA.csv', index = False)
+
 print("TESLA sentiments score are calculated")
+
 
 # Calculate GM tweets
 ticker_tweets_GM = pd.read_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/ticker_set_GM.csv')
 product_tweets_GM = pd.read_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/product_set_GM.csv')
 
-Stanford_sentiment = []
-for index, row in ticker_tweets_GM.iterrows():
-    text = Sentence(preprocess_tweet(row['content']))
-    text = str(text)
-    text = en_nlp(text)
-    score = sentence_sentiment_df(text)
-    Stanford_sentiment.append(score)
+get_stanford_sentiment(ticker_tweets_GM)
+get_textblob_sentiment(ticker_tweets_GM)
+get_flair_sentiment(ticker_tweets_GM)
 
-ticker_tweets_GM['Stanford_sentiment'] = Stanford_sentiment
+get_stanford_sentiment(product_tweets_GM)
+get_textblob_sentiment(product_tweets_GM)
+get_flair_sentiment(product_tweets_GM)
+
 ticker_tweets_GM.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/ticker_set_GM.csv', index = False)
-
-Stanford_sentiment = []
-for index, row in product_tweets_GM.iterrows():
-    text = Sentence(preprocess_tweet(row['content']))
-    text = str(text)
-    text = en_nlp(text)
-    score = sentence_sentiment_df(text)
-    Stanford_sentiment.append(score)
-
-product_tweets_GM['Stanford_sentiment'] = Stanford_sentiment
 product_tweets_GM.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Trading the twitter sentiment replica/Output/product_set_GM.csv', index = False)
+
 print("GM sentiments score are calculated")
