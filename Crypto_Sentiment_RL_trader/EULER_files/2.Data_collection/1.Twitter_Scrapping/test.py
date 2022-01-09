f10ffb091226ce72e759dfa80b2c2beaad01a277
fabianwin <fabian.winkelmann@outlook.com>
2022-01-09 16:41:37 +0100

    test 3

--- Crypto_Sentiment_RL_trader/EULER_files/2.Data_collection/1.Twitter_Scrapping/test.py
+++ Crypto_Sentiment_RL_trader/EULER_files/2.Data_collection/1.Twitter_Scrapping/test.py
@@ -5,4 +5,4 @@ df = pd.DataFrame(data=d)
 
 print(df)
 
-df.to_csv(r'Crypto_Sentiment_RL_trader/EULER_files/2.Data_collection/1.Twitter_Scrapping/Data/ticker_sets/test.csv')#SHIB_2021.csv
+df.to_csv(r'Master_thesis/Crypto_Sentiment_RL_trader/EULER_files/2.Data_collection/1.Twitter_Scrapping/Data/ticker_sets/test.csv')#SHIB_2021.csv
