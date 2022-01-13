7ab769dcdd4a0210c1fcda9279a30d21cba66ec9
fabianwin <fabian.winkelmann@outlook.com>
2022-01-13 22:58:51 +0100

    testing for EULER

---
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment Analysis/test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# initialize data of lists.
+data = {'Name':['Tom', 'nick', 'krish', 'jack'],
+        'Age':[20, 21, 19, 18]}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+df.to_csv(r'Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment Analysis/test.csv',index=False)
+
+df = pd.read_csv(r'Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment Analysis/test.csv')
+print(df.head())
