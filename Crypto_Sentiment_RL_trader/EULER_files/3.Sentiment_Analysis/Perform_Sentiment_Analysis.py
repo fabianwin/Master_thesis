90173861f19d0ef66e83bce23f7bcf57bd1c90b7
fabianwin <fabian.winkelmann@outlook.com>
2022-01-14 23:22:49 +0100

    Euler 14

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -5,7 +5,8 @@ import time
 
 def perform_sentiment_analysis(df):
     toc_0 = time.perf_counter()
-    df = get_stanford_sentiment(df)
+    #df = get_stanford_sentiment(df)
+    # does probably not work because email adress in one of the tweets! New preprocessing funciton should fix it
     toc_1 = time.perf_counter()
     print(f"Performed Stanford Sentiment  in {toc_1 - toc_0:0.4f} seconds")
 
