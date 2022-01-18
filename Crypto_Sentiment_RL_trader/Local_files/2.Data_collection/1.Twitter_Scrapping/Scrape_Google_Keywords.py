ee079b25529f258b5668b9d5c43b32d9b0fc37b2
fabianwin <fabian.winkelmann@outlook.com>
2022-01-19 00:06:14 +0100

    Design improvements for entire scrapping chapter

--- Crypto_Sentiment_RL_trader/Local_files/2.Data_collection/1.Twitter_Scrapping/Scrape_Google_Keywords.py
+++ Crypto_Sentiment_RL_trader/Local_files/2.Data_collection/1.Twitter_Scrapping/Scrape_Google_Keywords.py
@@ -1,6 +1,4 @@
-from functions import scrape_product_tweets, scrape_google_trendwords, scrape_google_trendwords_year
-import os
-import pandas as pd
+from functions import scrape_google_trendwords
 
 #define which coins we scrape twitter data, only look at coins in top20 marketgap (January 2022)
 #for product sets search for written out terms, than iterate through query findings
@@ -9,35 +7,8 @@ venture_capital_backed_coins = ['BINANCE', 'CARDANO', 'RIPPLE']
 community_driven_coins = ['DOGECOIN', 'SHIBA INU']
 coin_list = classic_coins + venture_capital_backed_coins + community_driven_coins
 
-"""
+
 for coin in coin_list:
     scrape_google_trendwords(coin)
 
-print("Product Set completely scraped")
-"""
-
-
-
-scrape_google_trendwords("BINANCE")
-
-
-"""
-#-----------Merge Datasets together
-dates = ['2021', '2020', '2019', '2018', '2017']
-
-def merge_Sets(keyword):
-    pdList = []
-    for year in dates:
-        my_path = os.path.abspath(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/2.Data_collection/1.Twitter_Scraping/Yearly_datasets/keywords_sets')
-        my_file = 'keyword_set_'+keyword+"_"+year+".csv"
-        df = pd.read_csv(os.path.join(my_path, my_file))
-        pdList.append(df)
-
-    entire_twitter_df = pd.concat(pdList)
-    my_path = os.path.abspath(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/2.Data_collection/1.Twitter_Scraping')
-    my_file = 'keyword_set_'+keyword+".csv"
-    entire_twitter_df.to_csv(os.path.join(my_path, my_file))
-
-
-merge_Sets("SHIBA INU")
-"""
+print("Google queries completely scraped")
--- Crypto_Sentiment_RL_trader/Local_files/2.Data_collection/1.Twitter_Scrapping/functions.py
+++ Crypto_Sentiment_RL_trader/Local_files/2.Data_collection/1.Twitter_Scrapping/functions.py
@@ -17,9 +17,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #############Global Parameters###################
 #set global parameters for twitterSearchscraper
 maxTweets = 10000000000000
-restrictions='min_faves:5 exclude:retweets lang:"en"' #min. 10 likes ,no retweets, in english
-#dates = ['2021', '2020', '2019', '2018', '2017']
-dates = ['2021', '2020', '2019', '2017']
+restrictions='min_faves:5 exclude:retweets lang:"en"' #min. 5 likes ,no retweets, in english
+dates = ['2021', '2020', '2019', '2018', '2017']
 ticker_col =["tweet_id","date_short","date_medium","date_long","username","content","likes","retweets","followers Count"]
 product_col =["tweet_id","date_short","username","content","likes","retweets","followers Count","keyword"]
 ticker_tweets_df = pd.DataFrame(columns=ticker_col)
@@ -103,7 +102,7 @@ def scrape_google_trendwords(keyword):
                 print(rising)
             time.sleep(7+random.random())
 
-        my_path = os.path.abspath(r'/Users/fabianwinkelmann/Desktop/Msc Thesis/Master_thesis/Crypto_Sentiment_RL_trader/Local_files/2.Data_collection/1.Twitter_Scrapping')
+        my_path = os.path.abspath(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/2.Data_collection/1.Twitter_Scraping/Yearly_datasets/keywords_sets')
         my_file = 'keyword_set_'+keyword+"_"+year+".csv"
         complete.to_csv(os.path.join(my_path, my_file))
 
@@ -111,40 +110,26 @@ def scrape_google_trendwords(keyword):
         print("there are", complete.shape[0]," unique combinations of keywords and dates")
 
 #---------------------
-#can be deleted after all keywords sets are scrapped
-def scrape_google_trendwords_year(keyword, year):
-    sdate = date(int(year),1,1)
-    edate = date(int(year),12,31)
-    full_date_list = pd.date_range(sdate,edate-timedelta(days=1),freq='d')
+def filter_google_queries(keyword,df_tweets):
+    len_list = list()
+    #load datasets
+    semantic_search_words = pd.read_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/2.Data_collection/1.Twitter_Scraping/Yearly_datasets/keywords_sets/keyword_filter.csv', usecols=[0], encoding='utf-8')
+    neg_exp = list(semantic_search_words['neg_words'].values.flatten())
+    #add special character strings
+    special_char = ['kaç tl','dólar','fiyatı','árfolyam', 'kaç','cotação','giá', 'fiyat grafi_i','yatƒ±rma','soru≈üturma',' –±–∏—Ä–∂–∞','ka√ß','fiyatƒ±','–∫—É—Ä—Å','davasƒ±']
+    neg_exp = neg_exp+special_char
+    # make a string with separator | from the list
+    neg_exp_string = "|".join(neg_exp)
 
-    #build the trendreq payload
-    pytrend = TrendReq(retries=2, backoff_factor=0.1, requests_args={'verify':False})
-    #provide your search terms
-    kw_list=[keyword]
-    #for every day we have a tweet in the ticker_set we look up the corresponding related queries on that day
-    complete = pd.DataFrame()
-    #build complete (set with all the related search words from google)
-    for i in full_date_list:
-        i = str(i.date())
-        date_local = i+" "+i
-        pytrend.build_payload(kw_list,timeframe=date_local, cat=0, geo='', gprop='')
-        #get related queries
-        related_queries = pytrend.related_queries()
-        #construct df with trend words
-        rising = list(related_queries.values())[0]['rising']
-        if rising is not None:
-            #print(rising)
-            rising['date'] = i
-            complete = complete.append(rising, ignore_index= True)
-            print(rising)
-        time.sleep(30+random.random())
+    # replace german word with english translation, choose Stock since this is the only relevant keyword which needs translation
+    df_tweets['query'] = df_tweets['query'].replace("aktie","stock",regex=True)
+    # drop duplicates
+    df_tweets.drop_duplicates(subset=['query','date'], keep='first', inplace=True)
+    df_tweets = df_tweets[~df_tweets['query'].str.contains(neg_exp_string, case=False)]
+    print('Shape after negative word filter: ' + str(len(df_tweets)))
+    len_list.append(len(df_tweets))
 
-    my_path = os.path.abspath(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/2.Data_collection/1.Twitter_Scraping/Yearly_datasets/keywords_sets')
-    my_file = 'keyword_set_'+keyword+"_"+str(year)+".csv"
-    complete.to_csv(os.path.join(my_path, my_file))
-
-    print("Google related queries were scraped and we know all the keywords for scraping the",keyword,"Product set in the year", year)
-    print("there are", complete.shape[0]," unique combinations of keywords and dates")
+    return df_tweets
 
 #---------------------
 def scrape_product_tweets(keyword):
@@ -152,10 +137,13 @@ def scrape_product_tweets(keyword):
     my_file = 'keyword_set_'+keyword+".csv"
     keyword_df = pd.read_csv(os.path.join(my_path, my_file))
 
+
     print(keyword_df.shape)
     keyword_df = keyword_df.dropna(axis=0, how="any")
-    keyword_df = keyword_df.head(3)
     print(keyword_df.shape)
+    keyword_df = filter_google_queries(keyword, keyword_df)
+    print(keyword_df.shape)
+
 
     #iterate over every row in the complete-set
     df = product_tweets_df
