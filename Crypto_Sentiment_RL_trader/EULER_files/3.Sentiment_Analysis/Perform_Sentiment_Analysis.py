9b4e531cefb760e79bb3a47d1b5f70af0c42d28d
fabianwin <fabian.winkelmann@outlook.com>
2022-01-20 23:15:06 +0100

    EULER BNB

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'ticker_set_preprocessed_#BTC.csv')
+df = pd.read_csv(r'ticker_set_preprocessed_#BNB.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'ticker_set_sentiment_#BTC.csv', index = False)
+df.to_csv(r'ticker_set_sentiment_#BNB.csv', index = False)
