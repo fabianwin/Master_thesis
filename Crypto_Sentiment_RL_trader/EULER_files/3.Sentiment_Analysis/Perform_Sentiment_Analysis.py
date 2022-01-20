6470468be43642724882a2e6261643410ef48ce4
fabianwin <fabian.winkelmann@outlook.com>
2022-01-20 23:39:40 +0100

    Euler shib

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'ticker_set_preprocessed_#DOGE.csv')
+df = pd.read_csv(r'ticker_set_preprocessed_#SHIB.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'ticker_set_sentiment_#DOGE.csv', index = False)
+df.to_csv(r'ticker_set_sentiment_#SHIB.csv', index = False)
