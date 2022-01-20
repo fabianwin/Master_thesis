881addb7f640e7b8d0d2d59ceb9f914988633ae5
fabianwin <fabian.winkelmann@outlook.com>
2022-01-20 23:35:38 +0100

    Euler sol

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'ticker_set_preprocessed_#ADA.csv')
+df = pd.read_csv(r'ticker_set_preprocessed_#SOL.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'ticker_set_sentiment_#ADA.csv', index = False)
+df.to_csv(r'ticker_set_sentiment_#SOL.csv', index = False)
