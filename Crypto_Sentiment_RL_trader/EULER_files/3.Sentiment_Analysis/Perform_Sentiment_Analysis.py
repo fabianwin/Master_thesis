bb1391f9fef60fe934196ec65e73a3b2987e95ba
fabianwin <fabian.winkelmann@outlook.com>
2022-01-20 23:07:49 +0100

    Euler 4

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -26,13 +26,11 @@ def perform_sentiment_analysis(df):
     #df = get_cardiffnlp_sentiment(df)
     toc_5 = time.perf_counter()
     print(f"Performed Cardiffnlp sentiment in {toc_5 - toc_4:0.4f} seconds")
-
     print(f"total time {toc_5 - toc_0:0.4f} seconds")
     return df
 
 
 #load dataframe
 df = pd.read_csv(r'ticker_set_preprocessed_#BTC.csv')
-df = df.head(5)
 df = perform_sentiment_analysis(df)
 df.to_csv(r'ticker_set_sentiment_#BTC.csv', index = False)
