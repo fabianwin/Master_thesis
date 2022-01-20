d05b332bd5576e5e1dd7d001a784f3755cbcc7aa
fabianwin <fabian.winkelmann@outlook.com>
2022-01-20 23:38:10 +0100

    Euler Doge

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'ticker_set_preprocessed_#ETH.csv')
+df = pd.read_csv(r'ticker_set_preprocessed_#DOGE.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'ticker_set_sentiment_#ETH.csv', index = False)
+df.to_csv(r'ticker_set_sentiment_#DOGE.csv', index = False)
