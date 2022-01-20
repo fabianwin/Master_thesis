da39af38b9dafe74f321926efac1cbe33a197480
fabianwin <fabian.winkelmann@outlook.com>
2022-01-20 23:36:18 +0100

    Euler ETH

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'ticker_set_preprocessed_#SOL.csv')
+df = pd.read_csv(r'ticker_set_preprocessed_#ETH.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'ticker_set_sentiment_#SOL.csv', index = False)
+df.to_csv(r'ticker_set_sentiment_#ETH.csv', index = False)
