1ecc1953c291ef779334d24a72505bbfcde3516f
fabianwin <fabian.winkelmann@outlook.com>
2022-01-25 00:24:07 +0100

    Euler 1

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'ticker_set_preprocessed_#BTC.csv.csv')
+df = pd.read_csv(r'ticker_set_preprocessed_#BTC.csv')
 df = perform_sentiment_analysis(df)
 df.to_csv(r'ticker_set_sentiment_#BTC.csv', index = False)
