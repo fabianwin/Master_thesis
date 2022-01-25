5a9ea628bed96e741b2375d3caea83217b369953
fabianwin <fabian.winkelmann@outlook.com>
2022-01-25 01:17:57 +0100

    Euler XRP

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'product_set_preprocessed_DOGECOIN.csv')
+df = pd.read_csv(r'product_set_preprocessed_RIPPLE.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'product_set_sentiment_DOGECOIN.csv', index = False)
+df.to_csv(r'product_set_sentiment_RIPPLE.csv', index = False)
