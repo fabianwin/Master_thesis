d23340ba835ca5e2abba90b2770fc94738ad6335
fabianwin <fabian.winkelmann@outlook.com>
2022-01-25 01:03:18 +0100

    Euler bnb

--- Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
+++ Crypto_Sentiment_RL_trader/EULER_files/3.Sentiment_Analysis/Perform_Sentiment_Analysis.py
@@ -31,6 +31,6 @@ def perform_sentiment_analysis(df):
 
 
 #load dataframe
-df = pd.read_csv(r'product_set_preprocessed_ETHEREUM.csv')
+df = pd.read_csv(r'product_set_preprocessed_BINANCE.csv')
 df = perform_sentiment_analysis(df)
-df.to_csv(r'product_set_sentiment_ETHEREUM.csv', index = False)
+df.to_csv(r'product_set_sentiment_BINANCE.csv', index = False)
