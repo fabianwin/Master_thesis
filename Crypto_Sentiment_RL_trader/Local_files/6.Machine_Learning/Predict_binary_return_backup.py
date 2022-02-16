d6413c1415b8b572c1cb18f87b626c2618bb023c
fabianwin <fabian.winkelmann@outlook.com>
2022-02-16 16:03:33 +0100

    Euler adjustments

--- Crypto_Sentiment_RL_trader/Local_files/6.Machine_Learning/Predict_binary_return_backup.py
+++ Crypto_Sentiment_RL_trader/Local_files/6.Machine_Learning/Predict_binary_return_backup.py
@@ -67,18 +67,34 @@ def LogReg_Pred(feature_list, coin, set, feature_df, predict_return_df):
     plt.savefig(os.path.join(my_path, my_file), format='pdf', bbox_inches='tight')
     """
 
-    print("shape of total df",df_not_imputed.shape)
+    df = missing_values_table(df_not_imputed)
+    print(df)
+
+    #create test set which was not imputed
+    #test set must be 20% --> 365
     print()
-    train, test = train_test_split(df_not_imputed, test_size=0.3, random_state=42)
-    print("train",train.shape)
-    print("test",test.shape)
-    #Imputer
+    feature_list_1 =  df.loc[df['Missing Values'] < 1000].index.values
+    print(len(feature_list))
+    print()
+    print(len(feature_list_1))
+
+    df_1 = feature_df.loc[:,feature_list]
+    df_1.dropna(axis=0, how='any',inplace=True)
+    print(df_1.shape)
+    #rest can be used for training
+
+
+
+
+    df_not_imputed.dropna(axis=0, how='any',inplace=True)
+    print(df_not_imputed.shape)
+
+
+    """
+    #iterative imputer
     imputer = IterativeImputer(random_state=42)
-    imputed = imputer.fit_transform(train)
-    train = pd.DataFrame(imputed, columns=df_not_imputed.columns)
-    print("Train NaN count = ",train.isna().sum().sum())
-    X_train = train.loc[:,feature_list]
-    y_train = pd.DataFrame(train.loc[:,'positive_return'])
+    imputed = imputer.fit_transform(df_not_imputed)
+    df_iterative_imputed = pd.DataFrame(imputed, columns=df_not_imputed.columns)
 
     #build a pipeline to find the best overall combination
     #define a standard scaler to normalize inputs
@@ -96,7 +112,7 @@ def LogReg_Pred(feature_list, coin, set, feature_df, predict_return_df):
     ])
 
     N_FEATURES_OPTIONS = list(range(1,len(feature_list)+1,1))
-    C_OPTIONS = np.logspace(-4,4,10)
+    C_OPTIONS = list = np.logspace(-4,4,10)
     reducer_labels = ['PCA', 'KBest(f_classif, k="all")']
 
     param_grid = [
@@ -116,14 +132,23 @@ def LogReg_Pred(feature_list, coin, set, feature_df, predict_return_df):
 
     #iterate over different imputation methods and scoring methods
     # tests have shown that iterative imputer is good enough and we want to optimize for accuracy.
-    dfs = [df_not_imputed]
+    dfs = [df_iterative_imputed]
     scores = ["accuracy"]
     for i, df in enumerate(dfs):
+        print(df.shape)
+        X = df.loc[:,feature_list]
+        Y = pd.DataFrame(df.loc[:,'positive_return'])
+        X_miss,Y_miss = missing_values_table(X), missing_values_table(Y)
+        print()
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3,random_state=42)
+
         for score in scores:
             print("# Tuning hyper-parameters for %s" % score)
             print()
+
             search = GridSearchCV(pipe, cv=5, scoring=score, param_grid = param_grid, n_jobs=processors)
             search.fit(X_train, y_train.values.ravel())
+
             print("Best parameters set found on development set:")
             print()
             print(search.best_params_)
@@ -135,136 +160,24 @@ def LogReg_Pred(feature_list, coin, set, feature_df, predict_return_df):
             for mean, std, params in zip(means, stds, search.cv_results_["params"]):
                 print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
             print()
-            print("Impute the test set")
-            imputer = IterativeImputer(random_state=42)
-            imputed = imputer.fit_transform(test)
-            test = pd.DataFrame(imputed, columns=df_not_imputed.columns)
-            X_test = test.loc[:,feature_list]
-            y_test = pd.DataFrame(test.loc[:,'positive_return'])
-            print()
 
-            print("Make the prediction")
             y_true, y_pred = y_test, search.predict(X_test)
             print(classification_report(y_true, y_pred))
             print()
+
             importance = search.best_estimator_[2].coef_[0]
             print(search.best_params_)
             for i,v in enumerate(importance):
             	print('Feature: %0d, Score: %.5f' % (i,v))
+
             result_df = pd.DataFrame.from_dict(search.cv_results_, orient='columns')
             new_row = {'columns':list(result_df.columns),'score': score,'Coin':coin,'Set_description': set,'supervised ML algorithm type':"Logistic Regression",'Features':feature_list,'Accuracy_Score':accuracy_score(y_true,y_pred), 'Precision_Score':precision_score(y_true,y_pred), 'Recall_Score':recall_score(y_true,y_pred), 'F1_Score':f1_score(y_true,y_pred),'Best_Parameters':search.best_params_}
             predict_return_df= predict_return_df.append(new_row, ignore_index=True)
+        """
 
     return predict_return_df
 #----------------------------
 def SVM_Pred(feature_list, coin, set, feature_df, predict_return_df):
-    #create X and Y datasets
-    feature_list.append('positive_return')
-    df_not_imputed = feature_df.loc[:,feature_list]
-    feature_list.remove('positive_return')
-
-    #display nullity
-    """
-    print(df_not_imputed.shape)
-    my_path = os.path.abspath(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/5.Statistical_Analysis/Daily_trading/Missing_Data')
-    msno.matrix(df_not_imputed,fontsize=5, figsize=(5, 5))
-    my_file = coin+ "_matrix_nullity"
-    plt.savefig(os.path.join(my_path, my_file), format='pdf', bbox_inches='tight')
-    msno.heatmap(df_not_imputed,fontsize=8, figsize=(5, 5))
-    my_file = coin+ "_heatmap_nullity"
-    plt.savefig(os.path.join(my_path, my_file), format='pdf', bbox_inches='tight')
-    msno.dendrogram(df_not_imputed,fontsize=8, figsize=(5, 5))
-    my_file = coin+ "_dendrogram_nullity"
-    plt.savefig(os.path.join(my_path, my_file), format='pdf', bbox_inches='tight')
-    """
-
-    print("shape of total df",df_not_imputed.shape)
-    print()
-    train, test = train_test_split(df_not_imputed, test_size=0.3, random_state=42)
-    print("train",train.shape)
-    print("test",test.shape)
-    #Imputer
-    imputer = IterativeImputer(random_state=42)
-    imputed = imputer.fit_transform(train)
-    train = pd.DataFrame(imputed, columns=df_not_imputed.columns)
-    print("Train NaN count = ",train.isna().sum().sum())
-    X_train = train.loc[:,feature_list]
-    y_train = pd.DataFrame(train.loc[:,'positive_return'])
-
-    #build a pipeline to find the best overall combination
-    #define a standard scaler to normalize inputs
-    std_slc = StandardScaler()
-    #create a classifier regularization
-    pca = PCA()
-    # set the tolerance to a large value to make the example faster
-    SVC = svm.SVC()
-
-    #create actual pipeline
-    pipe = Pipeline([
-        ('scaler', std_slc),
-        ('selector', "passthrough"),
-        ('classifier', SVC)
-    ])
-
-    N_FEATURES_OPTIONS = list(range(1,len(feature_list)+1,1))
-    C_OPTIONS =  [0.1, 0.35,0.4,0.45, 1, 10, 100, 1000, 10000, 100000, 1000000]
-    gamma = [1, 0.1, 0.01, 0.001, 0.0001]
-    reducer_labels = ['PCA']
-
-    param_grid = [
-        {
-            'selector': [PCA()],
-            'selector__n_components': N_FEATURES_OPTIONS,
-            'classifier__C': C_OPTIONS,
-            'classifier__gamma': gamma,
-            'classifier__kernel':['rbf']
-        }
-    ]
-
-    #iterate over different imputation methods and scoring methods
-    # tests have shown that iterative imputer is good enough and we want to optimize for accuracy.
-    dfs = [df_not_imputed]
-    scores = ["accuracy"]
-    for i, df in enumerate(dfs):
-        for score in scores:
-            print("# Tuning hyper-parameters for %s" % score)
-            print()
-            search = GridSearchCV(pipe, cv=5, scoring=score, param_grid = param_grid, n_jobs=processors)
-            search.fit(X_train, y_train.values.ravel())
-            print("Best parameters set found on development set:")
-            print()
-            print(search.best_params_)
-            print()
-            print("Grid scores on development set:")
-            print()
-            means = search.cv_results_["mean_test_score"]
-            stds = search.cv_results_["std_test_score"]
-            for mean, std, params in zip(means, stds, search.cv_results_["params"]):
-                print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
-            print()
-            print("Impute the test set")
-            imputer = IterativeImputer(random_state=42)
-            imputed = imputer.fit_transform(test)
-            test = pd.DataFrame(imputed, columns=df_not_imputed.columns)
-            X_test = test.loc[:,feature_list]
-            y_test = pd.DataFrame(test.loc[:,'positive_return'])
-            print()
-
-            print("Make the prediction")
-            y_true, y_pred = y_test, search.predict(X_test)
-            print(classification_report(y_true, y_pred))
-            print()
-            importance = search.best_estimator_[2].coef_[0]
-            print(search.best_params_)
-            for i,v in enumerate(importance):
-            	print('Feature: %0d, Score: %.5f' % (i,v))
-            result_df = pd.DataFrame.from_dict(search.cv_results_, orient='columns')
-            new_row = {'columns':list(result_df.columns),'score': score,'Coin':coin,'Set_description': set,'supervised ML algorithm type':"Logistic Regression",'Features':feature_list,'Accuracy_Score':accuracy_score(y_true,y_pred), 'Precision_Score':precision_score(y_true,y_pred), 'Recall_Score':recall_score(y_true,y_pred), 'F1_Score':f1_score(y_true,y_pred),'Best_Parameters':search.best_params_}
-            predict_return_df= predict_return_df.append(new_row, ignore_index=True)
-
-    return predict_return_df
-#----------------------------
-def SVM_Pred_copy(feature_list, coin, set, feature_df, predict_return_df):
     #create X and Y datasets
     feature_list.append('positive_return')
     df_not_imputed = feature_df.loc[:,feature_list]
@@ -379,6 +292,9 @@ def missing_values_table(df):
 coins=['ADA','BNB','BTC','DOGE','ETH', 'XRP']
 sets=["ticker", "product"]
 
+coins=['BTC']
+sets=["product"]
+
 predict_return = pd.DataFrame([], columns=['Coin','Set_description','supervised ML algorithm type','Features','Accuracy_Score', 'Precision_Score', 'Recall_Score', 'F1_Score'])
 for coin in coins:
     for set in sets:
@@ -389,8 +305,9 @@ for coin in coins:
         data_df = pd.read_csv(os.path.join(my_path, my_file), parse_dates=date_cols, dayfirst=True)
         data_df = add_return_boolean(data_df)
 
-        #run with features from Chen Paper (9 features)
-        feature_list_appendable = ["_number_of_tweets", "_finiteautomata_sentiment", "_finiteautomata_sentiment_expectation_value_volatility", "_average_number_of_followers"]
+
+        #run with features from Chen Paper
+        feature_list_appendable = ["_number_of_tweets", "_finiteautomata_sentiment", "_finiteautomata_sentiment_expectation_value_volatility", "_average_number_of_followers", "_finiteautomata_sentiment"]
         feature_list = [set + item for item in feature_list_appendable]
         if set == "ticker":
             feature_list.append("Momentum_14_ticker_finiteautomata_sentiment")
@@ -398,11 +315,11 @@ for coin in coins:
             feature_list.append("Momentum_14_product_finiteautomata_sentiment")
         feature_list.extend(["Real Volume","MOM_14","Volatility","RSI_14"])
 
-        #predict_return = LogReg_Pred(feature_list, coin, set, data_df, predict_return)
+        predict_return = LogReg_Pred(feature_list, coin, set, data_df, predict_return)
         #predict_return.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/6.Machine_Learning/return_logreg_predictions.csv', index = False)
 
-        predict_return = SVM_Pred(feature_list, coin, set, data_df, predict_return)
-        predict_return.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/6.Machine_Learning/return_SVM_predictions.csv', index = False)
+        #predict_return = SVM_Pred(feature_list, coin, set, data_df, predict_return)
+        #predict_return.to_csv(r'/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/6.Machine_Learning/return_SVM_predictions.csv', index = False)
 
         print("-------------------------------------")
 
