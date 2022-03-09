a7bcfce613f24026d9e937d5a95dbb37001e34e1
fabianwin <fabian.winkelmann@outlook.com>
2022-03-09 01:12:57 +0100

    euler

--- Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-ADA.py
+++ Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-ADA.py
@@ -475,7 +475,7 @@ if __name__ == "__main__":
     # finance features
     feature_list_3 = ["Circulating Marketcap", "Adjusted NVT","Adjusted RVT", "Sharpe Ratio", "Volatility", "MOM_14","RSI_14","pos_conf","neg_conf"]
     feature_list = mandatory_features + feature_list_3
-    #df = df.loc[:,feature_list]
+    df = df.loc[:,feature_list]
 
     # network features
     feature_list_4 = ["Median Transaction Fees", "Adjusted Transaction Volume", "Median Transfer Value","Transactions Count", "Active Supply", "Addresses Count", "Active Addresses Count", "Active Addresses Count (Received)","Active Addresses Count (Sent)","Addresses with balance greater than $1"]
@@ -485,7 +485,7 @@ if __name__ == "__main__":
     # all features togehter
     feature_list = ["date","Price (Open)","Price (High)","Price (Low)","Price (Close)","Real Volume"]
     feature_list_big = mandatory_features+ feature_list_1 + feature_list_2 + feature_list_3 + feature_list_4
-    df = df.loc[:,feature_list_big]
+    #df = df.loc[:,feature_list_big]
 
     ##################################
     df.fillna(method="ffill", inplace=True)
