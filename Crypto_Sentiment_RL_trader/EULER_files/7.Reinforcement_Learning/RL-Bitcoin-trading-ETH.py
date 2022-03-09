93beacbb9f7ebd77b997ff4123420011afdae9ad
fabianwin <fabian.winkelmann@outlook.com>
2022-03-09 14:47:42 +0100

    EUler

--- Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-ETH.py
+++ Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-ETH.py
@@ -38,8 +38,7 @@ class CustomAgent:
         self.action_space = np.array([0, 1, 2])
 
         # folder to save models
-        # folder to save models
-        path = "ETH_all/"
+        path = "ETH_finance/"
         self.log_name = path+datetime.now().strftime("%Y_%m_%d_%H_%M")+"_Crypto_trader"
 
         # State size contains Market+Orders+Indicators history for the last lookback_window_size steps
@@ -480,7 +479,7 @@ if __name__ == "__main__":
     # finance features
     feature_list_3 = ["Circulating Marketcap", "Adjusted NVT","Adjusted RVT", "Sharpe Ratio", "Volatility", "MOM_14","RSI_14","pos_conf","neg_conf"]
     feature_list = mandatory_features + feature_list_3
-    #df = df.loc[:,feature_list]
+    df = df.loc[:,feature_list]
 
     # network features
     feature_list_4 = ["Median Transaction Fees", "Adjusted Transaction Volume", "Median Transfer Value","Transactions Count", "Active Supply", "Addresses Count", "Active Addresses Count", "Active Addresses Count (Received)","Active Addresses Count (Sent)","Addresses with balance greater than $1"]
@@ -490,7 +489,7 @@ if __name__ == "__main__":
     # all features togehter
     feature_list = ["date","Price (Open)","Price (High)","Price (Low)","Price (Close)","Real Volume"]
     feature_list_big = mandatory_features+ feature_list_1 + feature_list_2 + feature_list_3 + feature_list_4
-    df = df.loc[:,feature_list_big]
+    #df = df.loc[:,feature_list_big]
 
     ##################################
     df.fillna(method="ffill", inplace=True)
@@ -520,7 +519,7 @@ if __name__ == "__main__":
 
     #finance features
     agent = CustomAgent(lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Finance Features")
-    #train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 32, training_batch_size=150, visualize=False, EPISODES=400000)
+    train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 32, training_batch_size=150, visualize=False, EPISODES=400000)
 
     # network features
     agent = CustomAgent(lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Network Features")
@@ -528,7 +527,7 @@ if __name__ == "__main__":
 
     # all features togehter
     agent = CustomAgent(lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="All features")
-    train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 32, training_batch_size=150, visualize=False, EPISODES=400000)
+    #train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 32, training_batch_size=150, visualize=False, EPISODES=400000)
 
     #train
     #test_multiprocessing(CustomEnv, CustomAgent, test_df, test_df_nomalized, num_worker = 10, visualize=False, test_episodes=1000, folder="/Users/fabianwinkelmann/Library/Mobile Documents/com~apple~CloudDocs/Master Thesis/Code/Crypto_Sentiment_RL_trader/7.Reinforcement_Learning/XRP/2022_03_08_19_30_Crypto_trader", name="1492.19_Crypto_trader", comment="3 months")
