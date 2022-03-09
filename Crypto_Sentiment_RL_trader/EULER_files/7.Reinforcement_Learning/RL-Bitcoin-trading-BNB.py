9a38a3f4f931a5f9951919e39e0dea9b4f48efd8
fabianwin <fabian.winkelmann@outlook.com>
2022-03-09 15:08:46 +0100

    Euler

--- Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-BNB.py
+++ Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-BNB.py
@@ -491,7 +491,7 @@ if __name__ == "__main__":
     feature_list = ["date","Price (Open)","Price (High)","Price (Low)","Price (Close)","Real Volume"]
     feature_list_big = mandatory_features+ feature_list_1 + feature_list_2 + feature_list_3 + feature_list_4
     df = df.loc[:,feature_list_big]
-
+    
     ##################################
     df.fillna(method="ffill", inplace=True)
     df = df.dropna()
