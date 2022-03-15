db60b5ee87137f1cbc391285152d733d096164b4
fabianwin <fabian.winkelmann@outlook.com>
2022-03-15 23:41:53 +0100

    euler

--- Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-BTC.py
+++ Crypto_Sentiment_RL_trader/EULER_files/7.Reinforcement_Learning/RL-Bitcoin-trading-BTC.py
@@ -38,7 +38,7 @@ class CustomAgent:
         self.action_space = np.array([0, 1, 2])
 
         # folder to save models
-        path = "BTC_ticker/"
+        path = "BTC_network/"
         self.log_name = path+datetime.now().strftime("%Y_%m_%d_%H_%M")+"_Crypto_trader"
 
         # State size contains Market+Orders+Indicators history for the last lookback_window_size steps
@@ -464,7 +464,7 @@ if __name__ == "__main__":
     mandatory_features = ["date","Price (Open)","Price (High)","Price (Low)","Price (Close)","Real Volume"]
     feature_list_1 = ["ticker_number_of_tweets", "ticker_average_number_of_likes", "ticker_average_number_of_retweets", "ticker_average_number_of_followers", "ticker_finiteautomata_sentiment","ticker_finiteautomata_sentiment_expectation_value_volatility","ROC_2_ticker_finiteautomata_sentiment","Momentum_14_ticker_finiteautomata_sentiment"]
     feature_list = mandatory_features + feature_list_1
-    df = df.loc[:,feature_list]
+    #df = df.loc[:,feature_list]
 
     # product sentiments
     feature_list_2 = ["product_number_of_tweets", "product_average_number_of_likes", "product_average_number_of_retweets", "product_average_number_of_followers", "product_finiteautomata_sentiment","product_finiteautomata_sentiment_expectation_value_volatility","ROC_2_product_finiteautomata_sentiment","Momentum_14_product_finiteautomata_sentiment"]
@@ -506,7 +506,7 @@ if __name__ == "__main__":
 
     #ticker sentiment
     agent = CustomAgent(lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Ticker Features")
-    train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 32, training_batch_size=150, visualize=False, EPISODES=100000)
+    #train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 32, training_batch_size=150, visualize=False, EPISODES=400000)
 
     #product sentiments
     agent = CustomAgent(lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Product Features")
